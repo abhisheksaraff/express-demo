1b6ccaf14e2cee36f02e82db2bc56a0bfaddd295
a6hi <saraffabhishek@outlook.com>
2024-12-26 17:10:22 -0500

    added view for homepage

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require("express");
+const path = require("node:path");
 const app = express();
 const authorRouter = require("./routes/authorRouter");
 const bookRouter = require("./routes/bookRouter");
 const indexRouter = require("./routes/indexRouter");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 
 // app.get("/", (req, res) => res.send("Hello World"));
 
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
 app.use("/", indexRouter);
+/*
 app.use((req, res, next) => {
   throw new Error("OH NO!");
   // or next(new Error("OH NO!"));
 });
+*/
 
 // Every thrown error in the application or the previous middleware function calling `next`
 // with an error as an argument will eventually go to this middleware function
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,9 +1,26 @@
 // /
 
-const {Router} = require("express");
+const { Router } = require("express");
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => res.send("Home"));
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
+indexRouter.get("/", (req, res) => {
+  res.render("index", { links: links, users: users });
+});
+
+//indexRouter.get("/", (req, res) => res.send("Home"));
+/*
+indexRouter.get("/", (req, res) =>
+  res.render("index", { message: "Home from EJS!" })
+);
+*/
+
 indexRouter.get("/about", (req, res) => res.send("About"));
 indexRouter.get("/contact", (req, res) => res.send("Gettng Contact"));
 indexRouter.post("/contact", (req, res) => res.send("Posting Contact"));
